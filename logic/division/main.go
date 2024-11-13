f097b26b4bb680985c25f3cf3b2665c5bc67e6e1
Alpamys Kanibetov <alpamys.kanibetov@gmail.com>
2024-11-13 19:32:23 +0500

    scores of matches

--- logic/division/main.go
+++ logic/division/main.go
@@ -15,38 +15,55 @@ func main() {
 }
 
 type Team struct {
-	id   int
-	name string
+	id            int
+	name          string
+	points        int
+	goalsScored   int
+	goalsConceded int
+	wins          int
+	draws         int
+	loses         int
+}
+
+func (t *Team) diff() int {
+	return t.goalsScored - t.goalsConceded
 }
 
 type Match struct {
-	id         int
-	firstTeam  *Team
-	secondTeam *Team
+	id              int
+	firstTeam       *Team
+	secondTeam      *Team
+	firstTeamScore  int
+	secondTeamScore int
+	winnerId        int
 }
 
 func (m *Match) name() string {
 	return fmt.Sprintf("%s - %s", m.firstTeam.name, m.secondTeam.name)
 }
 
+func (m *Match) score() string {
+	return fmt.Sprintf("%d - %d", m.firstTeamScore, m.secondTeamScore)
+}
+
 func divideTeamsIntoDivisions() {
 	var teams = []*Team{
-		{1, "Liverpool"},
-		{2, "Arsenal"},
-		{3, "Aston Villa"},
-		{4, "Milan"},
-		{5, "Juventus"},
-		{6, "Barcelona"},
-		{7, "Bayern Munchen"},
-		{8, "Borussia Dortmund"},
-		{9, "Manchester City"},
-		{10, "Chelsea"},
-		{11, "Manchester United"},
-		{12, "Inter milan"},
-		{13, "Atalanta"},
-		{14, "Real Madrid"},
-		{15, "Atletico Madrid"},
-		{16, "Bayer Leverkusen"},
+		{1, "Liverpool", 0, 0, 0, 0, 0, 0},
+		{2, "Arsenal", 0, 0, 0, 0, 0, 0},
+		{3, "Aston Villa", 0, 0, 0, 0, 0, 0},
+		{4, "Milan", 0, 0, 0, 0, 0, 0},
+		{5, "Juventus", 0, 0, 0, 0, 0, 0},
+		{6, "Barcelona", 0, 0, 0, 0, 0, 0},
+		{7, "Bayern Munchen", 0, 0, 0, 0, 0, 0},
+		{8, "Borussia Dortmund", 0, 0, 0, 0, 0, 0},
+		{9, "Manchester City", 0, 0, 0, 0, 0, 0},
+		{10, "Chelsea", 0, 0, 0, 0, 0, 0},
+		{11, "Manchester United", 0, 0, 0, 0, 0, 0},
+		{12, "Inter milan", 0, 0, 0, 0, 0, 0},
+		{13, "Atalanta", 0, 0, 0, 0, 0, 0},
+		{14, "Real Madrid", 0, 0, 0, 0, 0, 0},
+		{15, "Atletico Madrid", 0, 0, 0, 0, 0, 0},
+		{16, "Bayer Leverkusen", 0, 0, 0, 0, 0, 0},
 	}
 
 	fmt.Println("teams:")
@@ -82,14 +99,14 @@ func divideTeamsIntoDivisions() {
 
 func generateMatches() { // assume taking 8 teams
 	var teams = []*Team{
-		{1, "Liverpool"},
-		{2, "Arsenal"},
-		{3, "Aston Villa"},
-		{4, "Milan"},
-		{5, "Juventus"},
-		{6, "Barcelona"},
-		{7, "Bayern Munchen"},
-		{8, "Borussia Dortmund"},
+		{1, "Liverpool", 0, 0, 0, 0, 0, 0},
+		{2, "Arsenal", 0, 0, 0, 0, 0, 0},
+		{3, "Aston Villa", 0, 0, 0, 0, 0, 0},
+		{4, "Milan", 0, 0, 0, 0, 0, 0},
+		{5, "Juventus", 0, 0, 0, 0, 0, 0},
+		{6, "Barcelona", 0, 0, 0, 0, 0, 0},
+		{7, "Bayern Munchen", 0, 0, 0, 0, 0, 0},
+		{8, "Borussia Dortmund", 0, 0, 0, 0, 0, 0},
 	}
 
 	fmt.Println("teams:")
@@ -116,6 +133,9 @@ func generateMatches() { // assume taking 8 teams
 					id,
 					v,
 					v2,
+					0,
+					0,
+					-1,
 				})
 				m[key] = true
 				id++
@@ -209,6 +229,48 @@ func generateMatches() { // assume taking 8 teams
 
 	fmt.Println("\nscheduled matches:")
 	for i, v := range scheduledMatches {
-		fmt.Println(i+1, v.id, v.name())
+		fmt.Println(i+1, v.id, v.name()) // , v.score()
 	}
+
+	// generate scores of matches
+	maxGoals := 5 // assume max goals
+	for _, v := range scheduledMatches {
+		v.firstTeamScore = rand.Intn(maxGoals)
+		v.secondTeamScore = rand.Intn(maxGoals)
+
+		if v.firstTeamScore > v.secondTeamScore {
+			v.winnerId = v.firstTeam.id
+			v.firstTeam.points += 3
+			v.firstTeam.wins++
+			v.secondTeam.loses++
+		} else if v.firstTeamScore < v.secondTeamScore {
+			v.winnerId = v.secondTeam.id
+			v.secondTeam.points += 3
+			v.secondTeam.wins++
+			v.firstTeam.loses++
+		} else {
+			v.firstTeam.points++
+			v.secondTeam.points++
+			v.secondTeam.draws++
+			v.firstTeam.draws++
+		}
+
+		v.firstTeam.goalsScored += v.firstTeamScore
+		v.secondTeam.goalsScored += v.secondTeamScore
+
+		v.firstTeam.goalsConceded += v.secondTeamScore
+		v.secondTeam.goalsConceded += v.firstTeamScore
+	}
+
+	fmt.Println("\nscores of matches:")
+	for i, v := range scheduledMatches {
+		fmt.Println(i+1, v.id, v.name(), v.score())
+	}
+
+	fmt.Println("\nteam points:")
+	for i, v := range teams {
+		fmt.Println(i+1, fmt.Sprintf("id %d, name %s, W %d, D %d, L %d, F %d, A %d, GD %d, P %d", v.id, v.name, v.wins, v.draws, v.loses, v.goalsScored, v.goalsConceded, v.diff(), v.points))
+	}
+
+	// leave sorting algorithm to db, points, then diff, then if you have same teams look at the matches between???
 }
