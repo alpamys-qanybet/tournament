87f31e01ba7725466bc7634b519f7f7cb22aed38
Alpamys Kanibetov <alpamys.kanibetov@gmail.com>
2024-11-13 13:30:55 +0500

    generate matches of a division, both twice or once, shuffle of the result will not work, because some teams play in a raw, they must have time to relax

--- logic/division/main.go
+++ logic/division/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/google/uuid"
 	"golang.org/x/exp/rand"
 )
 
@@ -99,13 +98,92 @@ func generateMatches() { // assume taking 8 teams
 
 	fmt.Println(teams)
 
-	teamsSlice := make([]string, 0)
-	teamsSlice = append(teamsSlice, teams[:]...)
+	// teamsSlice := make([]string, 0)
+	// teamsSlice = append(teamsSlice, teams[:]...)
 
-	a := GenerateRoundRobinTournamentMatchesByTeams(teamsSlice)
-	fmt.Println(a)
+	// a := GenerateRoundRobinTournamentMatchesByTeams(teamsSlice) // from internet
+	// fmt.Println(a)
+
+	// matches := make([]string, 0)
+	matchesOnce := make([]string, 0)
+	m := make(map[string]bool)
+
+	for i, v := range teams {
+		for j, v2 := range teams {
+			if i == j {
+				continue
+			}
+			// matches = append(matches, fmt.Sprintf("%d %s - %d %s", i, v, j, v2))
+
+			key := fmt.Sprintf("%d_%d", i, j)
+			_, ok := m[key]
+			keyReverse := fmt.Sprintf("%d_%d", j, i)
+			_, ok2 := m[keyReverse]
+
+			if !(ok || ok2) { // none exists
+				// matchesOnce = append(matchesOnce, fmt.Sprintf("%d %s - %d %s", i, v, j, v2))
+				matchesOnce = append(matchesOnce, fmt.Sprintf("%s - %s", v, v2))
+				m[key] = true
+			}
+		}
+	}
+
+	// fmt.Println(matches) // each other home - away
+	// for i, v := range matches {
+	// 	fmt.Println(i+1, v)
+	// }
+	// (7 * 8) = 56 matches // correct
+
+	for i, v := range matchesOnce {
+		fmt.Println(i+1, v)
+	}
+	// 28 matches // correct
+
+	/*
+		r := rand.New(rand.NewSource(uint64(time.Now().UnixNano())))
+		r.Shuffle(len(matchesOnce), func(i, j int) { // shuffle matches(re-order with random indexes)
+			matchesOnce[i], matchesOnce[j] = matchesOnce[j], matchesOnce[i]
+		})
+
+		for i, v := range matchesOnce {
+			fmt.Println(i+1, v)
+		}
+	*/
+
+	/*
+		shuffling will not works, because some teams play in a raw, we need some logic here to give each team a time to relax
+		1 Liverpool - Aston Villa
+		2 Aston Villa - Juventus
+		3 Juventus - Bayern Munchen
+		4 Arsenal - Milan
+		5 Juventus - Barcelona
+		6 Milan - Bayern Munchen
+		7 Aston Villa - Bayern Munchen
+		8 Aston Villa - Barcelona
+		9 Aston Villa - Milan
+		10 Arsenal - Juventus
+		11 Arsenal - Bayern Munchen
+		12 Liverpool - Bayern Munchen
+		13 Liverpool - Arsenal
+		14 Milan - Barcelona
+		15 Liverpool - Barcelona
+		16 Arsenal - Barcelona
+		17 Liverpool - Juventus
+		18 Bayern Munchen - Borussia Dortmund
+		19 Arsenal - Borussia Dortmund
+		20 Juventus - Borussia Dortmund
+		21 Aston Villa - Borussia Dortmund
+		22 Barcelona - Bayern Munchen
+		23 Liverpool - Borussia Dortmund
+		24 Milan - Juventus
+		25 Barcelona - Borussia Dortmund
+		26 Arsenal - Aston Villa
+		27 Milan - Borussia Dortmund
+		28 Liverpool - Milan
+	*/
 }
 
+/*
 // GenerateRoundRobinTournamentMatches generates a 2d slice of matches of a single round robin tournament.
 // Each team will play one time against all other teams.
 func GenerateRoundRobinTournamentMatchesByTeams(teams []string) [][]string {
@@ -159,3 +237,4 @@ func stringSlicecontains(s []string, e string) bool {
 	}
 	return false
 }
+*/
